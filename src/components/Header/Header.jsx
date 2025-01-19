fe6deb4ace08519d1721d9693b090b56276ed36d
Reynaldo Perez Pauli <reynaldoperezpauli6@gmail.com>
2025-01-18 23:53:45 -0600

    Latest

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import './Header.css';
 import {Link, useLocation} from 'react-router-dom';
+import logo from '../../images/logo.jpeg';
 
 function Header() {
     const location = useLocation();
 
     return (
         <header className='header'>
-            <img src="https://logos-world.net/wp-content/uploads/2020/05/Kansas-City-Royals-Logo.png" className='header__image'/>
+            <img src={logo} className='header__image'/>
 
             <div className='header__right-column'>
                 <Link to='/' className={`header__link ${location.pathname==='/'?'header__link_active':''}`}>Home</Link>
